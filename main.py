6bdb5d10c38e266e4292c9c3424a99749e6f21d3
Prince032017 <pt13081999@gmail.com>
2020-05-27 09:42:19 +0530

    sixth

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
-batch_size = 32
+batch_size = 10
 num_classes = 10
 epochs = 1
 
